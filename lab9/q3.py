dc4e47518acf660b13db5c48d4da4455ed9bb881
Sophie <sophietanker@gmail.com>
2022-11-03 18:52:55 -0400

    better comments for lab9/q3.py

--- lab9/q3.py
+++ lab9/q3.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
 
+#opening pUC19c file, creating a list of sequences.
 seq = open('pUC19c.fasta')
 lines = seq.readlines()
 sequence = []
 for i in range (0, len(lines)):
     if lines[i][0] != '>':
         sequence.append(lines[i].strip("\n"))
-    
+
+#joining all sequences in the list together.
 Sequence = " ".join(sequence)
 Sequence = Sequence.rstrip("\t")
 
+#counting amount of SMAI sequences
 SMAI = Sequence.count("CCCGGG")
 print(str(SMAI) + " SMAI sequences.")
+
+#AT and GC counts
 AT = Sequence.count("A") + Sequence.count("T")
 print("Amount of AT: " + str(AT))
 GC = Sequence.count("G") + Sequence.count("C")
